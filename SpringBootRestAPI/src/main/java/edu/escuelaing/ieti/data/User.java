439544329a4eb6318578aa93157697f516cd67f7
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-19 17:54:59 -0500

    Código Fuente

--- SpringBootRestAPI/src/main/java/edu/escuelaing/ieti/data/User.java
+++ SpringBootRestAPI/src/main/java/edu/escuelaing/ieti/data/User.java
@@ -1,4 +1,15 @@
 package edu.escuelaing.ieti.data;
 public class User {
-    
+    private final String id;
+    private final String name;
+    private final String email;
+    private final String lastName;
+    private final String createdAt;
+    public User(String id, String name, String email, String lastName, String createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.lastName = lastName;
+        this.createdAt = createdAt;
+    }
 }
