d50ab3128d3ce650898343279a1eab99e327dfdb
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-19 18:26:26 -0500

    Código Fuente

--- SpringBootRestAPI/src/main/java/edu/escuelaing/ieti/data/User.java
+++ SpringBootRestAPI/src/main/java/edu/escuelaing/ieti/data/User.java
@@ -1,15 +1,46 @@
 package edu.escuelaing.ieti.data;
+import java.util.Date;
 public class User {
-    private final String id;
-    private final String name;
-    private final String email;
-    private final String lastName;
-    private final String createdAt;
+    private String id;
+    private String name;
+    private String email;
+    private String lastName;
+    private Date createdAt;
     public User(String id, String name, String email, String lastName, String createdAt) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.lastName = lastName;
-        this.createdAt = createdAt;
+        this.createdAt = new Date();
+    }
+    public String getId(){
+        return this.id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public Date getCreatedAt(){
+        return this.createdAt;
+    }
+    public void setCreatedAt(String createdAt){
+        this.createdAt = new Date();
     }
 }
--- SpringBootRestAPI/src/main/java/edu/escuelaing/ieti/service/UserService.java
+++ SpringBootRestAPI/src/main/java/edu/escuelaing/ieti/service/UserService.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.escuelaing.ieti.service;
-
-/**
- *
- * @author skull
- */
+import edu.escuelaing.ieti.data.User;
+import java.util.List;
 public interface UserService {
+    
+    User create( User user );
+
+    User findById( String id );
+        
+    List<User> all();
+
+    void deleteById( String id );
+
+    User update( User user, String userId );
     
 }
