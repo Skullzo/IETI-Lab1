7f26ef9d3a00629d4be679b8ac3559c9058d7e5f
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-22 14:16:36 -0500

    Source Code for Part 2.

---
+++ Part 2/src/main/java/edu/escuelaing/ieti/APIRestApplication.java
@@ -0,0 +1,9 @@
+package edu.escuelaing.ieti;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class APIRestApplication {
+    public static void main(String[] args) {
+	SpringApplication.run(APIRestApplication.class, args);
+    }
+}
