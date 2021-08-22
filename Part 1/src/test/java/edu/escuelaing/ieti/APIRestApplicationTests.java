05efb3fac5d36f47b648c1530cee533bf292540a
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-22 13:49:57 -0500

    Source Code for Part 1.

---
+++ Part 1/src/test/java/edu/escuelaing/ieti/APIRestApplicationTests.java
@@ -0,0 +1,9 @@
+package edu.escuelaing.ieti;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class APIRestApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
