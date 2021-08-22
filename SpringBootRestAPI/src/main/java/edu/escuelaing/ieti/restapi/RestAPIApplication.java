3e04bef395cddbaf01ae68223cf6960758757607
Alejandro Toro Daza <skullzo@outlook.com>
2021-08-22 02:16:47 -0500

    Código Fuente

--- SpringBootRestAPI/src/main/java/edu/escuelaing/ieti/restapi/RestAPIApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.escuelaing.ieti.restapi;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class RestAPIApplication {
-    public static void main(String[] args) {
-	SpringApplication.run(RestApiApplication.class, args);
-    }
-}
